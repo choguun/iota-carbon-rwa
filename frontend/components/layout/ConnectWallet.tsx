eb6230ce5a4dbf47d2000e671739858bb7d1408e
choguun <visaruth.s@gmail.com>
2025-05-01 10:45:02 +0700

    update: ui

---
+++ frontend/components/layout/ConnectWallet.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import { ConnectButton } from "@iota/dapp-kit";
+import React from 'react';
+
+function ConnectWallet() {
+  return (
+    <ConnectButton />
+  );
+}
+
+export default ConnectWallet; 

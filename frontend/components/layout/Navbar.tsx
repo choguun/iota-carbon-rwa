dd99b51df0bda755b9a8c1bacd57518d06d509e0
choguun <visaruth.s@gmail.com>
2025-05-03 17:41:35 +0700

    Update Navbar.tsx

--- frontend/components/layout/Navbar.tsx
+++ frontend/components/layout/Navbar.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
 import ConnectWallet from './ConnectWallet'; // Assuming ConnectWallet is in the same directory
-import { WalletStatus } from './WalletStatus';
 
 const Navbar = () => {
   return (
